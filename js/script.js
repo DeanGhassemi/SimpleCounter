a54939c17bce652fb39d979580a4decd8f24d6b0
Dean <deang467@gmail.com>
2023-05-11 12:58:06 -0400

    dark mode and responsive design

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let count = 0;
 document.getElementById("button").onclick = counter = () =>{
     console.log(count)
-    document.getElementById("clickCount").innerHTML = `Number of times clicked: ${count++}`;
+    document.getElementById("clickCount").innerHTML = `Number of times clicked: ${++count}`;
 }
